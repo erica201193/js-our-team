5a7d5d729e229f74e7c5655a51385b470ed0e9bd
erica201193 <erica.mancini20@gmail.com>
2022-06-01 15:23:01 +0200

    Milestone 1 Completed

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const listaMembriTeam = [
+    {
+        nome: "Joe",
+        cognome: "Bastianich",
+        ruolo: "Founder & CEO",
+    },
+    {
+        nome: "Mara",
+        cognome: "Maionchi",
+        ruolo: "General Manager",
+    },
+    {
+        nome: "Bruno",
+        cognome: "Barbieri",
+        ruolo: "Administration Finance & Control Executive"
+    },
+    {
+        nome: "Francesca",
+        cognome: "Michielin",
+        ruolo: "HR Admin"
+    },
+    {
+        nome: "Antonino",
+        cognome: "Cannavacciuolo",
+        ruolo: "Engineering Food Sales Manager"
+    },
+    {
+        nome: "Britney",
+        cognome: "Spears",
+        ruolo: "Account Manager"
+    },
+    {
+        nome: "Elon",
+        cognome: "Musk",
+        ruolo: "Engineering Innovation Manager"
+    },
+    {
+        nome: "Jeff",
+        cognome: "Bezos",
+        ruolo: "Tax consultant"
+    },
+]
+
+for (let i = 0; i < listaMembriTeam.length; i++) {
+    const datiMembri = listaMembriTeam[i];
+
+    console.log(datiMembri.nome, datiMembri.cognome + ` - ${datiMembri.ruolo}`);
+}
